e02213557807e8898102a88c11104283329d5fc0
Dorian Szekely <dorian.szekely@gmail.com>
2016-08-22 19:19:19 +0200

    removed frontend stuff

--- gulp/tasks/build-html.js
+++
@@ -1,9 +0,0 @@
-var config = require('../config');
-var gulp = require('gulp');
-var path = require('path');
-
-exports.task = function() {
-    var source = gulp.src('./source/index.html');
-    
-    return source.pipe(gulp.dest(path.join(config.outputDir, '')));
- };
