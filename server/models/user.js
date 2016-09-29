f46a7d235a2a3ead088f922e7b4df09a07ae569b
d4nSource <dorian.szekely@gmail.com>
2016-09-29 07:37:29 +0200

    add gulp

---
+++ server/models/user.js
@@ -0,0 +1,8 @@
+var User = {
+    id: Number,
+    firstName: String,
+    lastName: String,
+    password: String,
+    email: String
+};
+module.exports = User;
---
+++ server/routes/user.js
@@ -0,0 +1,18 @@
+"use strict";
+var express = require('express');
+var sqlite3 = require('sqlite3').verbose();
+var passwordHash = require('password-hash');
+var router = express.Router(); // get an instance of the express Router
+// var User = require('../models/user');
+var db = new sqlite3.Database('./database/database.sqlite3');
+// get all users
+router.get('/', function (req, res, next) {
+    var users;
+    db.each("SELECT id, firstname, lastname, email FROM users where id=0", function (err, row) {
+        //users= {"firstname": row.firstname, "lastname": row.lastname, "email": row.email};
+        res.send(row.firstname);
+    });
+    //    db.run("INSERT into users VALUES ('2', 'Max', 'Mustermann', 'geheim', 'test@Mustermann.com')");
+    db.close;
+});
+module.exports = router;
